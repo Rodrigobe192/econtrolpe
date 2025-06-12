3063904777ab30dd162f4834cc36a7de6c820124
Rodrigobe192 <benitesrodrigo425@gmail.com>
2025-06-11 23:42:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -577,18 +577,19 @@ app.post('/api/send', express.json(), async (req, res) => {
 
   try {
     await axios.post(
-      \`https://graph.facebook.com/v22.0/\${process.env.PHONE_NUMBER_ID}/messages\`, 
-      {
-        messaging_product: "whatsapp",
-        to,
-        text: { body: message },
-      },
-      {
-        headers: {
-          Authorization: \`Bearer \${process.env.WHATSAPP_TOKEN}\`
-        }
-      }
-    );
+      await axios.post(
+  `https://graph.facebook.com/v22.0/${process.env.PHONE_NUMBER_ID}/messages`, 
+  {
+    messaging_product: "whatsapp",
+    to,
+    text: { body: message },
+  },
+  {
+    headers: {
+      Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`
+    }
+  }
+);
 
     // Guardar mensaje del asesor
     if (!conversations[to]) conversations[to] = { responses: [] };
